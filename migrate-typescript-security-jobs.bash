04c3216e4451defcb63bc0dd7dd0f39b3d31c9c3
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-09-24 17:37:35 +0100

    SDIT-2050: 🐛 Fix migrate scripts after directory moved

--- migrate-typescript-security-jobs.bash
+++ migrate-typescript-security-jobs.bash
@@ -19,10 +19,10 @@ yq -i 'del(.workflows.security) | del(.workflows.security-weekly)' .circleci/con
 mkdir -p .github/workflows
 mkdir -p .github/scripts
 
-cp -a ../hmpps-github-actions/templates/security_npm_dependency.yml .github/workflows
-cp -a ../hmpps-github-actions/templates/security_trivy.yml .github/workflows
-cp -a ../hmpps-github-actions/templates/security_vera*.yml .github/workflows
-cp -a ../hmpps-github-actions/templates/scripts/auditjson_to_sarif.py .github/scripts
+cp -a ../hmpps-github-actions/templates/workflows/security_npm_dependency.yml .github/workflows
+cp -a ../hmpps-github-actions/templates/workflows/security_trivy.yml .github/workflows
+cp -a ../hmpps-github-actions/templates/workflows/security_vera*.yml .github/workflows
+cp -a ../hmpps-github-actions/templates/workflows/scripts/auditjson_to_sarif.py .github/scripts
 
 RANDOM_HOUR=$((RANDOM % (9 - 3 + 1) + 3))
 RANDOM_MINUTE=$((RANDOM%60))
