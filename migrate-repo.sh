2300b65b1e4f755f798ef7b39bb851ccf8825993
james Hart <james.hart@digital.justice.gov.uk>
2024-12-24 16:41:39 +0000

    new version of migrate repo script for validation

--- migrate-repo.sh
+++ migrate-repo.sh
@@ -98,15 +98,86 @@ migrate_node_security_jobs() {
 }
 
 migrate_kotlin_deployment_jobs() {
-  yq -i 'del(.workflows.build-test-and-deploy)' .circleci/config.yml
+
   mkdir -p .github/workflows
-  gh api repos/ministryofjustice/hmpps-template-kotlin/contents/templates/workflows/pipeline.yml -H "Accept: application/vnd.github.v3.raw" > .github/workflows/deployments.yml
+  pipeline_file=".github/workflows/pipeline.yml"
+  # Pull down the template pipeline and remove deploy_dev
+  gh api repos/ministryofjustice/hmpps-template-kotlin/contents/.github/workflows/pipeline.yml -H "Accept: application/vnd.github.v3.raw"  | grep -v  "^ *#" | yq eval 'del(.jobs.deploy_dev)' > ${pipeline_file}
+
+# load the list of build / test / deploy jobs into a string
+# only build-test-deploy will be populated - the others will be added at the bottom and commented out
+  all_btd_jobs=$(yq eval '.workflows | keys | map(select(test("build|test|deploy")))' .circleci/config.yml | awk '{print $2}')
+
+# Create a list of environments that are deployed to
+  workflow_jobs=$(yq eval '.workflows.build-test-and-deploy.jobs.[]' .circleci/config.yml)
+  deploy_envs=$(yq eval '.workflows.build-test-and-deploy | select(.jobs[]."hmpps/deploy_env") | .jobs[] | select(has("hmpps/deploy_env")) | ."hmpps/deploy_env".env' .circleci/config.yml)
+
+for each_env in $deploy_envs; do
+  echo "Migrating deployment job for $each_env"
+  env_params=$(yq eval '.workflows.build-test-and-deploy | select(.jobs[]."hmpps/deploy_env") | .jobs[] | select(has("hmpps/deploy_env")) | select(."hmpps/deploy_env".env == "'$each_env'")' .circleci/config.yml) 
+
+# add the parameters one at a time
+  echo "deploy_${each_env}:" >> ${pipeline_file}
+  echo "  name: Deploy to the ${each_env} environment" >> ${pipeline_file}
+
+# branches filter
+  branch_filter=$(echo $env_params | yq .'hmpps/deploy_env.filters.branches.only[]')
+  if [ -n "${branch_filter}" ]; then
+    echo -n "  if: github.ref in [" >> ${pipeline_file}
+    nb=0
+    for each_branch in $(echo $branch_filter); do
+      if [ $nb -eq 0 ] ; then
+        then nb=1 
+        else echo -n ',' 
+      fi
+      echo -n "'refs/heads/${each_branch}'"
+    done >> ${pipeline_file}
+    echo "]" >> ${pipeline_file}
+  fi
+
+  echo "  needs:" >> ${pipeline_file}
+  echo "  - build" >> ${pipeline_file}
+  echo "  - helm_lint" >> ${pipeline_file}
+
+  # create the needs for non-dev environments
+  if [ {each_env} != "dev" ]; then
+    needs=$(echo "${workflow_jobs}" | yq eval '.workflows.build-test-and-deploy.jobs.[]' .circleci/config.yml | yq ".request-${each_env}-approval.requires[] | select(test(\"^deploy_\"))") 
+    if [ -n "${needs}" ]; then
+      echo "  - ${needs}" >> ${pipeline_file}
+    fi
+  fi
+    echo "  uses: ministryofjustice/hmpps-github-actions/.github/workflows/deploy_env.yml@v2 # WORKFLOW_VERSION" >> ${pipeline_file}
+    echo "  secrets: inherit" >> ${pipeline_file}
+
+    echo "  with:" >> ${pipeline_file}
+    echo "    environment: '${each_env}'" >> ${pipeline_file}
+    echo "    app_version: '${{ needs.build.outputs.app_version }}'" >> ${pipeline_file}
+  # helm_timeout
+  if [ -n "$(echo "${env_params}" | yq eval '.helm_timeout')" ]; then
+    echo "  helm_timeout: $(echo "${env_params}" | yq eval '.helm_timeout')" >> ${pipeline_file}
+  fi
+  # helm_dir
+  if [ -n "$(echo "${env_params}" | yq eval '.helm_dir')" ]; then
+    echo "  helm_dir: $(echo "${env_params}" | yq eval '.helm_dir')" >> ${pipeline_file}
+  fi
+  # helm_additional_args
+  if [ -n "$(echo "${env_params}" | yq eval '.helm_additional_args')" ]; then
+    echo "  helm_additional_args: $(echo "${env_params}" | yq eval '.helm_additional_args')" >> ${pipeline_file}
+  fi
+
+done
+
+# Delete the build-test-and-deploy workflow when it's all done
+#  yq -i 'del(.workflows.build-test-and-deploy)' .circleci/config.yml
+
 }
 
 migrate_node_deployment_jobs() {
-  yq -i 'del(.workflows.build-test-and-deploy)' .circleci/config.yml
+  yq -i 'del(.workflows[] | select(.key | contains("deploy"))' .circleci/config.yml
+  yq -i 'del(.workflows | with_entries(select(.key | contains("build"))))' .circleci/config.yml
+  yq -i 'del(.workflows | with_entries(select(.key | contains("test"))))' .circleci/config.yml
   mkdir -p .github/workflows
-  gh api repos/ministryofjustice/hmpps-template-typescript/contents/templates/workflows/pipeline.yml -H "Accept: application/vnd.github.v3.raw" > .github/workflows/pipeline.yml
+  gh api repos/ministryofjustice/hmpps-template-typescript/contents/.github/workflows/pipeline.yml -H "Accept: application/vnd.github.v3.raw" > .github/workflows/pipeline.yml
 }
 
 
@@ -135,6 +206,8 @@ else
   exit 0
 fi
 
+# backup circleCC config
+cp .circleci/config.yml .circleci/config.yml.bak.$(date +%Y%m%d_%H%M%S)
 
 if [[ -f "package.json" ]]; then
   if [[ $selection && 1 ]]; then
